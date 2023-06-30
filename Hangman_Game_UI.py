76df36611d46fdafd2e9872e4766059f6aef9e1d
Vast-Emptiness <neagumarinel2014@gmail.com>
2023-06-30 22:27:38 +0300

    I can t put the hangman ascii when you are choosing wrong letter!

--- Hangman_Game_UI.py
+++ Hangman_Game_UI.py
@@ -11,15 +11,17 @@ def hello():
 
 # The game where all the action happens
 def hangman():
-    level = 0
-    lives = 6
+    guessed = []
+
+    lives = 0
     word = RandomWord()
     word_random = word.word(word_max_length=10, word_min_length=3, include_categories=['nouns'])  # random words
     # specification
+    print(word_random)
     word_stage = ' _' * len(word_random)
     print(word_stage)
 
-    while lives > 0:
+    while 0 <= lives < 6:
         letter_player = input('Choose a letter: ').strip().lower()
 
         if letter_player not in string.ascii_letters:
@@ -28,30 +30,33 @@ def hangman():
 
         if letter_player in word_stage:
             print('You have already guessed that letter. Just type again!')
+            guessed.append(letter_player)
+            print('Letter guessed by you:', ' '.join(guessed).capitalize())
+            print(word_stage)
             continue
 
         found = False
+
         for i in range(len(word_random)):
             if letter_player == word_random[i]:
                 word_stage = word_stage[:i] + letter_player + word_stage[i + 1:]
-                found = True
-                print(HANGMANPICS[level])
+        found = True
+        print(HANGMANPICS[lives])
 
         if found:
             print(word_stage)
         else:
-            lives -= 1
-            level += 1
-            print(HANGMANPICS[level])
+
+            lives += 1
+            print(HANGMANPICS[lives])
             print(word_stage)
-            print('Wrong letter! Try again.')
-            print('Lives left:', lives)
+            print('Wrong letter! Try again: ')
 
         if '_' not in word_stage:
             print('Congratulations! You guessed the word:', word_random)
             break
     else:
-        print('You lost the game :(')
+        print('You lost the game :(!')
 
 
 # The main function
@@ -62,7 +67,6 @@ def main():
         choice = input('Please just type y/n: ').strip().lower()
 
     if choice == 'y' or choice == 'yes':
-        print(HANGMANPICS[0])
         hangman()
     elif choice == 'n' or choice == 'no':
         print('Goodbye!')
@@ -82,3 +86,4 @@ if __name__ == '__main__':
         elif play_again == 'no' or play_again == 'n':
             print('Goodbye, man!')
             break
+    # hangman()
