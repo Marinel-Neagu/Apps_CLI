b6588ae6f581e0e8ba9816b4048143767fd5ded1
Marinel-Neagu <neagumarinel2014@gmail.com>
2023-07-18 18:24:54 +0300

    the logic is hard, just modified

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-- 👋 Hi, I’m @Marinel-Neagu
-- 👀 I’m interested in programing and i am a newbie :)
-- 🌱 I’m currently learning Python
-- 💞️ I’m looking to collaborate my programing skills
-- 📫 How to reach me...just look in bio
-
+ 
--- Tic_Tac_Toe_UI.py
+++ Tic_Tac_Toe_UI.py
@@ -33,8 +33,11 @@ def greetings():
     return start_game
 
 
-def board():
+def game_mode():
+    pass
 
+
+def board():
     board_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     board_scheme = (f'''
                 {board_matrix[0][0]} | {board_matrix[0][1]} | {board_matrix[0][2]}
@@ -48,7 +51,7 @@ def board():
 
 
 def print_board(matrix):
-    print (f'''
+    print(f'''
             {matrix[0][0]} | {matrix[0][1]} | {matrix[0][2]}
            ___|___|___
             {matrix[1][0]} | {matrix[1][1]} | {matrix[1][2]}
@@ -57,33 +60,95 @@ def print_board(matrix):
         ''')
 
 
-
-
 def player_choice():
-    board_ = board()
+    while True:
 
-    player_move = int(input('Chose a number from 1 to 9: ').strip())
-    if player_move == 1:
-        board_[0][0]='X'
-    elif player_move == 2:
-        board_[0][1] = 'X'
-    elif player_move == 3:
-        board_[0][2] = 'X'
-    print(board_)
-    print_board(board_)
-
-
-
-
-def computer_choice():
-    pass
+        player = input('Choose a number from 1-9:').strip()
+        if player.isdigit():
+            player = int(player)
+            if player in range(1, 10, 1):
+                break
+            else:
+                print('Invalid number, please choose a number form 0 to 9! ')
+        else:
+            print('Invalid number! ')
+    return player
 
 
-def game_mode():
-    pass
+def computer_choice(x):
+    computer = random.choice(range(1, 10, 1))
+    if computer not in x:
+        return computer
 
 
 def game():
+    board_ = board()
+    invalid_moves = list()
+    while True:
+        player_move = player_choice()
+
+        if player_move not in invalid_moves:
+            if player_move == 1:
+                board_[0][0] = 'X'
+            elif player_move == 2:
+                board_[0][1] = 'X'
+            elif player_move == 3:
+                board_[0][2] = 'X'
+            elif player_move == 4:
+                board_[1][0] = 'X'
+            elif player_move == 5:
+                board_[1][1] = 'X'
+            elif player_move == 6:
+                board_[1][2] = 'X'
+            elif player_move == 7:
+                board_[2][0] = 'X'
+            elif player_move == 8:
+                board_[2][1] = 'X'
+            elif player_move == 9:
+                board_[2][2] = 'X'
+            invalid_moves.append(player_move)
+            computer_move = computer_choice(invalid_moves)
+            print(computer_move)
+            if computer_move == 1:
+                board_[0][0] = 'O'
+                print(board_)
+            elif computer_move == 2:
+                board_[0][1] = 'O'
+                print(board_)
+            elif computer_move == 3:
+                board_[0][2] = 'O'
+                print(board_)
+            elif computer_move == 4:
+                board_[1][0] = 'O'
+                print(board_)
+            elif computer_move == 5:
+                board_[1][1] = 'O'
+                print(board_)
+            elif computer_move == 6:
+                board_[1][2] = 'O'
+                print(board_)
+            elif computer_move == 7:
+                board_[2][0] = 'O'
+                print(board_)
+            elif computer_move == 8:
+                board_[2][1] = 'O'
+                print(board_)
+            elif computer_move == 9:
+                board_[2][2] = 'O'
+                print(board_)
+            print_board(board_)
+        else:
+            print('Invalid move! Try again')
+            print_board(board_)
+
+
+
+
+def valid_move():
+    pass
+
+
+def checking_win(matrix):
     pass
 
 
@@ -96,4 +161,4 @@ def main():
 
 
 if __name__ == '__main__':
-    player_choice()
+    game()
