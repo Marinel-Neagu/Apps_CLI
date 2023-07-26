79549d40e45f70c346ca59085e473f7ef6d916f7
Marinel-Neagu <neagumarinel2014@gmail.com>
2023-07-26 18:00:35 +0300

    Updated the game and to make the code more clear

--- Currency_Converter_UI.py
+++ Currency_Converter_UI.py
@@ -8,11 +8,12 @@ for key, value in currency.items():
     print(key, ':', value)
 print('Hi, please tell me how many money do you want to convert!')
 money = int(input('Please press here the sum:').strip())
-start_currency = input('Please chose the currency do you have:').strip().capitalize()
-end_currency = input('Please chose the currency do you to tranform:').strip().capitalize()
+start_currency = input('Please chose the currency do you have:').strip().upper()
+end_currency = input('Please chose the currency do you to tranform:').strip().upper()
 
 if start_currency in currency and end_currency in currency:
-    for key, value in currency:
-        conversion = (start_currency[value] * money)*end_currency[valueL]
+    conversion = (currency[start_currency] * money)/currency[end_currency]
 
-        print('Here is your value:',conversion)
+    print('Here is your value:', conversion)
+else:
+    print('Please choose a good one!')
