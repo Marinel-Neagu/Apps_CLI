8e28229adf49c3b1f6abda28367d44f9e4edd2a7
Marinel-Neagu <neagumarinel2014@gmail.com>
2023-08-02 14:38:54 +0300

    .d

--- Unit_Converter_UI.py
+++ Unit_Converter_UI.py
@@ -1,1 +1,24 @@
 #  unit convertor, to choose to convert a unit to another unit
+unit_list = {
+	'exa': 10 ** 18,
+	'peta': 10 ** 15,
+	'tera': 10 ** 12,
+	'giga': 10 ** 9,
+	'mega': 10 ** 6,
+	'kilo': 10 ** 3,
+	'hecto': 10 ** 2,
+	'deca': 10 ** 1,
+	'deci': 10 ** -1,
+	'centi': 10 ** -2,
+	'milli': 10 ** -3,
+	'micro': 10 ** -6,
+	'nano': 10 ** -9,
+	'pico': 10 ** -12,
+	'femto': 10 ** -15,
+	'atto': 10 ** -18
+	
+}
+
+def conversion(unit1, unit2, value):
+	pass
+	
