147ef2562fd20859067a6349d9a9d519f2bb6093
Vast-Emptiness <137677636+Vast-Emptiness@users.noreply.github.com>
2023-08-07 22:09:16 +0300

    The program is done

--- Palindrome_Checker_UI.py
+++ Palindrome_Checker_UI.py
@@ -1,13 +1,12 @@
-# import re
-
-
 def user_word():
-	word_ = input('Please isert here the word to see if is a Palindrome: ').strip().lower()
-	word2 = word_[::-1]
-	if word2 == word_:
-		print('This word is a palindrome!')
-	else:
-		print('This is not a palindrome word!')
+	while True:
+		word_ = input('Please isert here the word to see if is a Palindrome: ').strip().lower()
+		word2 = word_[::-1]
+		if word2 == word_:
+			print('This word is a palindrome!')
+		else:
+			print('This is not a palindrome word!')
+
 
 if __name__ == '__main__':
 	user_word()
