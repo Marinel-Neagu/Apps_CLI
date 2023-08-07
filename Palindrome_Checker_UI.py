af79ad5fc0ee049e52ea66f2a492e47f646d6107
Marinel-Neagu <neagumarinel2014@gmail.com>
2023-08-07 18:41:52 +0300

    cleaning the code

--- Palindrome_Checker_UI.py
+++ Palindrome_Checker_UI.py
@@ -1,1 +1,13 @@
-improt fcntl afj af
+# import re
+
+
+def user_word():
+	word_ = input('Please isert here the word to see if is a Palindrome: ').strip().lower()
+	word2 = word_[::-1]
+	if word2 == word_:
+		print('This word is a palindrome!')
+	else:
+		print('This is not a palindrome word!')
+
+if __name__ == '__main__':
+	user_word()
