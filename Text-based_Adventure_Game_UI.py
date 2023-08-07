17f7984a609dd2d0be10ff87a3c54f23793ecb05
Marinel-Neagu <neagumarinel2014@gmail.com>
2023-08-07 18:17:33 +0300

    cleaning the code

--- Text-based_Adventure_Game_UI.py
+++ Text-based_Adventure_Game_UI.py
@@ -2,14 +2,14 @@ import time
 
 
 def welcome():
-	print('Welcome, you are a new owenr of a misterious game named,"The dark soul"')
+	print('Welcome, you are a new owner of a mysterious game named, “The dark soul.”')
 	time.sleep(2)
-	print('The game is from your grandpa, that died in a war that was almost 30 years ago')
+	print('The game is from your grandpa, that died in a war that was almost 30 years ago.')
 	time.sleep(2)
 	print('Now the game that he made is in your hand')
 	time.sleep(2)
-	print('You have to choose what to do it with! Or you give that game to your dad, or you open the game')
-	print('To open it write open')
+	print('You have to choose what to do it with! Or you give that game to your dad, or you open the game.')
+	print('To open it write open and to give to your dad write dad:')
 	time.sleep(2)
 	game_choice = input('Press here...')
 	return game_choice in ['open']
@@ -19,16 +19,16 @@ def game_world():
 	while True:
 		game_welcome = welcome()
 		if game_welcome:
-			print('You are throw away into a new world, you are now in the video game!')
+			print('You are thrown away into a new world, you are now in the video game!')
 			time.sleep(3)
-			print('What are you doing now: Try to cry or looking at yourself--')
+			print('What are you doing now: Try to cry or look at yourself--')
 			world_choice = input('Press here...').strip().lower()
 			if world_choice == 'cry' or world_choice == 'run':
-				print('You are now like a baby boy, and nothing is happening but then...')
+				print('You are now like a baby boy, and nothing is happening, but then ...')
 				time.sleep(3)
-				print('You are seeing a woman that is in black and with blood in her hands')
+				print('You are seeing a woman that is in black and with blood in her hands.')
 				time.sleep(3)
-				print('Do you want to ask the woman or run')
+				print('Do you want to ask the woman or run?')
 		
 		else:
 			print('Goodbye!')
