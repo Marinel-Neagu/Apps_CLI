bb2ac2fc03958eb7c351148964779af1d07a7be6
Vast-Emptiness <137677636+Vast-Emptiness@users.noreply.github.com>
2023-08-09 00:01:37 +0300

    Need to work more for add new futures

--- Currency_Converter_UI.py
+++ Currency_Converter_UI.py
@@ -1,39 +1,38 @@
 def currency1():
-    while True:
-        cur1 = input('Please chose the currency do you have:').strip().upper()
-        if cur1 in currency:
-            return cur1
-        else:
-            print('Please choose from the list!')
+	while True:
+		cur1 = input('Please chose the currency do you have:').strip().upper()
+		if cur1 in currency:
+			return cur1
+		else:
+			print('Please choose from the list!')
 
 
 def currency2():
-    while True:
-        cur2 = input('Please chose the currency do you have:').strip().upper()
-        if cur2 in currency:
-            return cur2
-        else:
-            print('Please choose from the list!')
+	while True:
+		cur2 = input('Please chose the currency do you want to change to:').strip().upper()
+		if cur2 in currency:
+			return cur2
+		else:
+			print('Please choose from the list!')
 
 
 def amount():
-    amount = input('Hi, please tell me how many money do you want to convert!')
+	number = input('Hi, please tell me how many money do you want to convert!')
+	return number
 
-currency = {
-    'USD': 1,
-    'EUR': 0.92,
-    'RON': 4.54,
-    'GBP': 0.79
-}
+
+currency = {'USD': 1, 'EUR': 0.92, 'RON': 4.54, 'GBP': 0.79}
 for key, value in currency.items():
-    print(key, ':', value)
+	print(key, ':', value)
 
 while True:
-
-    if start_currency in currency and end_currency in currency:
-        conversion = (currency[start_currency] * money) / currency[end_currency]
-
-        print('Here is your value:', conversion)
-        break
-    else:
-        print('Please choose a good one!')
+	start_currency = currency1()
+	end_currency = currency2()
+	money = amount()
+	if start_currency in currency and end_currency in currency:
+		conversion = (currency[start_currency] * money) / currency[end_currency]
+		
+		print('Here is your value:', conversion)
+		break
+	else:
+		print('Please choose a good one!')
--- Text-based_Adventure_Game_UI.py
+++ Text-based_Adventure_Game_UI.py
@@ -1,39 +1,64 @@
 import time
 
+'''
+the start of the game
+'''
+
+
+def choice_checking_valid():
+	while True:
+		answer = input('Your choice is:').strip().lower()
+		if answer == ' ':
+			print('Please enter a answer, do not let this question emtpy!')
+		else:
+			return answer
+
 
 def welcome():
 	print('Welcome, you are a new owner of a mysterious game named, “The dark soul.”')
 	time.sleep(2)
 	print('The game is from your grandpa, that died in a war that was almost 30 years ago.')
 	time.sleep(2)
-	print('Now the game that he made is in your hand')
+	print('Now the game that he made is in your hand!')
 	time.sleep(2)
 	print('You have to choose what to do it with! Or you give that game to your dad, or you open the game.')
-	print('To open it write open and to give to your dad write dad:')
+	player_welcome = choice_checking_valid()
 	time.sleep(2)
-	game_choice = input('Press here...')
-	return game_choice in ['open']
+	
+	return player_welcome
 
 
-def game_world():
-	while True:
-		game_welcome = welcome()
-		if game_welcome:
-			print('You are thrown away into a new world, you are now in the video game!')
+def enter_world():
+	game_welcome = welcome()
+	if game_welcome == 'open':
+		print('You are thrown away into a new world, you are now in the video game!')
+		time.sleep(3)
+		print('What are you doing now: Start cry or watching the surrounding?')
+		world_choice = choice_checking_valid()
+		if world_choice == 'cry' or world_choice == 'run':
+			print('You are now like a baby boy, and nothing is happening, but then ...')
 			time.sleep(3)
-			print('What are you doing now: Try to cry or look at yourself--')
-			world_choice = input('Press here...').strip().lower()
-			if world_choice == 'cry' or world_choice == 'run':
-				print('You are now like a baby boy, and nothing is happening, but then ...')
-				time.sleep(3)
-				print('You are seeing a woman that is in black and with blood in her hands.')
-				time.sleep(3)
-				print('Do you want to ask the woman or run?')
-		
-		else:
-			print('Goodbye!')
-			break
+			print('You are seeing a woman that is in black and with blood in her hands.')
+			time.sleep(3)
+			print('Do you want to ask the woman or run?')
+	elif game_welcome == 'throw away':
+		time.sleep(1)
+		print('You throw away the game, now you hear something from it....')
+		time.sleep(3)
+		print('The sound is coming form the game that you throw in trash can')
+		time.sleep(3)
+		print('And you see.... a black man with yellow eyes....')
+		time.sleep(2)
+		print('He is in a black coat, what are you gonna do?   ')
+		print('Are you gonna run to your dad or asking the man?')
+	
+	else:
+		print('Goodbye!')
+
+
+def meet_the_characters():
+	print('ahhah')
 
 
 if __name__ == '__main__':
-	game_world()
+	enter_world()
