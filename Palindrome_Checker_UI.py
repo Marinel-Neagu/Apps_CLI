a6885458e0a67ee6e012d0932eeb90aa00397c56
Marinel-Neagu <neagumarinel2014@gmail.com>
2023-08-10 16:56:32 +0300

    The game is done. Is very easy.

--- Palindrome_Checker_UI.py
+++ Palindrome_Checker_UI.py
@@ -2,13 +2,15 @@ import re
 
 
 def palindrome_checker(user_word):
-	clean_word = re.sub(r'[^a-zA0-9]', '', user_word)
+	clean_word = re.sub(r'[^a-zA-Z0-9]', '', user_word)
 	clean_word = clean_word.lower()
 	return clean_word == clean_word[::-1]
 
 
-sentence = input('Please put a sentence, or a word to see is it a palindrome: ').strip()
-if palindrome_checker(sentence):
-	print('Wow, this is a palindrome word!')
-else:
-	print('Ops, this is not a palindrome word!')
+while True:
+	
+	sentence = input('Please put a sentence, or a word to see is it a palindrome: ').strip()
+	if palindrome_checker(sentence):
+		print('Wow, this is a palindrome word!')
+	else:
+		print('Ops, this is not a palindrome word!')
