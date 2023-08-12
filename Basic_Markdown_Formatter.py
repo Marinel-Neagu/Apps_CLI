6c9336b5e7b891a61e35a31a8631be2867c28537
Vast-Emptiness <137677636+Vast-Emptiness@users.noreply.github.com>
2023-08-12 22:57:49 +0300

    Done, the logic is done

--- Basic_Markdown_Formatter.py
+++ Basic_Markdown_Formatter.py
@@ -1,6 +1,21 @@
 import re
 
-patterns = {r'\bfirst_name\b': 'Michel', r'\blast_name\b': 'Jackson', r'\bjob\b': 'Lazy Mann'}
+patterns = {
+	r'\bfirst_name\b': 'Michel',
+	r'\blast_name\b': 'Jackson',
+	r'\bjob\b': 'Lazy Mann'}
+
+
+def new_main():
+	"""
+Ask the user for a new sentence
+	"""
+	while True:
+		new = input('Do you want to add a new sentence, press y/yes or n/no to exit: ')
+		if new not in ['yes', 'y']:
+			break
+		else:
+			main()
 
 
 def sentence():
@@ -17,7 +32,7 @@ def main():
 	"""
 		Take the pattern and the new word from the patterns' dictionary and print it out.
 	"""
-	user_sentence = sentence() # discord helped a lot :)
+	user_sentence = sentence()  # discord helped a lot :)
 	for key, value in patterns.items():
 		user_sentence = re.sub(key, value, user_sentence)
 	print('Here is your modified sentence:', user_sentence)
@@ -25,3 +40,4 @@ def main():
 
 if __name__ == '__main__':
 	main()
+	new_main()
