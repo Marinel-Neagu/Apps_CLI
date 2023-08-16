bc86d00a19dee91be994f125dfb9a3416a7d869d
Marinel-Neagu <neagumarinel2014@gmail.com>
2023-08-16 14:30:12 +0300

    The program is done and is good.

--- Simple_Email_Extractor_UI.py
+++ Simple_Email_Extractor_UI.py
@@ -2,6 +2,10 @@ import re
 
 
 def get_document_user():
+	"""
+
+	:return: get the name of the document
+	"""
 	document_text = None
 	while not document_text:
 		document_text = input('Please insert the document name: ').strip().capitalize()
@@ -9,6 +13,11 @@ def get_document_user():
 
 
 def read_document_user(document):
+	"""
+
+	:param document:
+	:return: get the text of the document, and it removes the space after the text.
+	"""
 	with open(file=f'{document}.txt') as file:
 		text_document = file.read()
 		for lines in text_document:
@@ -16,12 +25,25 @@ def read_document_user(document):
 	return text_document
 
 
+# def clean_text(text):
+# 	clear = re.sub(r'/s+',' ',text)
+# 	return clear
+
 def email_pattern(document_text):
-	email = re.findall(r'[a-zA-Z0-9_+.%-]*@[a-zA-Z0-9]*\.[a-zA-Z]{2,6}',document_text)
+	"""
+
+	:param document_text:
+	:return: email pattern using regex module
+	"""
+	email = re.findall(r'[a-zA-Z0-9_+.%-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,6}', document_text)
 	return email
 
 
 def new_email_request():
+	"""
+
+	:return: ask the user for putting out the emails from the document
+	"""
 	new_request = None
 	while not new_request:
 		new_request = input('Type yes/y to find emails form a document or no/n to quit: ')
@@ -29,15 +51,26 @@ def new_email_request():
 
 
 def main():
-	while True:
-		if new_email_request():
-			document_user = get_document_user()
-			document_text = read_document_user(document_user)
-			valid_email = email_pattern(document_text)
-			print(valid_email)
-		else:
-			print('Goodbye!')
-			break
+	"""
+This program just extracts all valid emails form a text
+	"""
+	try:
+		while True:
+			num = 0
+			if new_email_request():
+				document_user = get_document_user()
+				document_text = read_document_user(document_user)
+				valid_email = email_pattern(document_text)
+				print('Here are the all emails that I found:')
+				for email in valid_email:
+					num += 1
+					print(f'Email NO {num}: {email}')
+			
+			else:
+				print('Goodbye, and see you out there!')
+				break
+	except FileNotFoundError:
+		print('Sorry, the name or the file is not here.')
 
 
 if __name__ == '__main__':
