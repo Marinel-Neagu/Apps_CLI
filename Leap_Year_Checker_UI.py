65deafee483762df0860b7bfe83b128607c46ae0
Marinel-Neagu <neagumarinel2014@gmail.com>
2023-08-30 16:11:45 +0300

    Need to polish the program

---
+++ Leap_Year_Checker_UI.py
@@ -0,0 +1,38 @@
+def get_year_user():
+	year = None
+	while not year:
+		year = input('What is the year you want to check: ').strip()
+	return year
+
+
+def check_leap_year(year):
+	if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+		print('This is a leap year!')
+	else:
+		print('This is not a leap year')
+
+
+def user_choice():
+	user_input = None
+	while not user_input:
+		user_input = input('Types yes to find a file size or n/no to quit: ').strip().lower()
+	return user_input in ['yes', 'y']
+
+
+def main():
+	while True:
+		try:
+			choice = user_choice()
+			if choice:
+				user_year = get_year_user()
+				check_leap_year(year=int(user_year))
+			
+			else:
+				print('Goodbye!')
+				break
+		except ValueError:
+			print('Sorry, you need to put a number!')
+
+
+if __name__ == '__main__':
+	main()
