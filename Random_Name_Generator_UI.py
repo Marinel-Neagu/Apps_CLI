af046449e1aa2fb18c3dd393d062d12c441075ef
Marinel-Neagu <neagumarinel2014@gmail.com>
2023-09-04 18:43:33 +0300

    Need to polish the program

--- Random_Name_Generator_UI.py
+++ Random_Name_Generator_UI.py
@@ -1,5 +1,12 @@
-def name():
-	 = [
+import random
+
+
+def name_boy_list():
+	"""
+
+	:return: the most popular names for the boys
+	"""
+	list_of_name = [
 		
 		"Wade", "Dave", "Seth", "Ivan", "Riley", "Gilbert", "Jorge", "Dan", "Brian", "Roberto", "Ramon", "Miles",
 		"Liam", "Nathaniel", "Ethan", "Lewis", "Milton", "Claude", "Joshua", "Glen", "Harvey", "Blake", "Antonio",
@@ -13,3 +20,70 @@ def name():
 	
 	]
 	
+	random_name = random.choice(list_of_name)
+	return random_name
+
+
+def surname_list():
+	"""
+
+	:return:list of the most popular surname in English
+	"""
+	list_of_name = [
+		
+		"Williams", "Harris", "Thomas", "Robinson", "Walker", "Scott", "Nelson", "Mitchell", "Morgan", "Cooper",
+		"Howard", "Davis", "Miller", "Martin", "Smith", "Anderson", "White", "Perry", "Clark", "Richards", "Wheeler",
+		"Warburton", "Stanley", "Holland", "Terry", "Shelton", "Miles", "Lucas", "Fletcher", "Parks", "Norris",
+		"Newton", "Potter", "Francis", "Erickson", "Norman", "Moody", "Lindsey", "Gross", "Sherman", "Simon", "Jones",
+		"Brown", "Garcia", "Rodriguez", "Lee", "Young", "Hall", "Allen", "Lopez", "Green", "Gonzalez", "Baker",
+		"Perez",
+		"Campbell", "Shaw", "Gordon", "Burns", "Warren", "Long", "Mcdonald", "Gibson", "Ellis", "Fisher", "Reynolds",
+		"Jordan", "Hamilton", "Ford", "Graham", "Griffin", "Russell", "Foster", "Butler", "Simmons", "Flores",
+		"Bennett", "Sanders", "Hughes", "Bryant", "Patterson", "Matthews", "Jenkins", "Watkins", "Ward", "Murphy",
+		"Bailey", "Bell", "Cox", "Martinez", "Evans", "Rivera", "Peterson", "Gomez", "Murray", "Tucker", "Hicks",
+		"Crawford", "Mason", "Rice", "Black", "Knight", "Arnold", "Wagner", "Mosby", "Ramirez", "Coleman", "Powell",
+		"Singh", "Patel", "Wood", "Wright", "Stephens", "Erikson", "Cook", "Roberts", "Holmes", "Kennedy", "Saunders",
+		"Fisher", "Hunter", "Reid", "Stewart", "Carter", "Phillips", "Spencer", "Howell", "Alvarez", "Little",
+		"Jacobs",
+		"Foreman", "Knowles", "Meadows", "Richmond", "Valentine", "Dudley", "Woodward", "Weasley", "Livingston",
+		"Sheppard", "Kimmel", "Noble", "Leach", "Gentry", "Lara", "Pace", "Trujillo", "Grant", "Guzman", "Daniel",
+		"Adams"]
+	
+	random_name = random.choice(list_of_name)
+	return random_name
+
+
+def name_girl_list():
+	"""
+
+	:return:list of the most popular girl name in English
+	"""
+	list_of_name = [
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	]
+	random_name = random.choice(list_of_name)
+	return random_name
