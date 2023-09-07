5ab99048eea7421ee2f5859dd4209a7fdda103c4
Marinel-Neagu <neagumarinel2014@gmail.com>
2023-09-07 18:35:09 +0300

    Need to polish the program

--- Count_Vowels_and_Consonants_UI.py
+++ Count_Vowels_and_Consonants_UI.py
@@ -1,16 +1,17 @@
-class CountVowelsConsonants:
+# oop program
+class countvowelsconsonants:
 	def __init__(self, sentence):
 		self.sentence = sentence.lower()
 		self.vowels = 'aeiou'
 		self.consonants = 'bcdfghjklmnpqrstvwxyz'
-	
+
 	def count_vowels(self):
 		count_vowels = 0
 		for letter in self.sentence:
 			if letter in self.vowels:
 				count_vowels += 1
 		return count_vowels
-	
+
 	def count_consonants(self):
 		count_consonants = 0
 		for letter in self.sentence:
@@ -22,7 +23,7 @@ class CountVowelsConsonants:
 def sentence_user():
 	sentence = None
 	while not sentence:
-		sentence = input('Insert here a sentence, or a word: ').lower()
+		sentence = input('Insert here a sentence, or a word: ').strip()
 	return sentence
 
 
@@ -31,10 +32,10 @@ def main():
 	while True:
 		sentence = sentence_user()
 		if sentence != 'quit':
-			counting = CountVowelsConsonants(sentence=sentence)
+			counting = countvowelsconsonants(sentence=sentence)
 			print(f'''
-			Total consonants: {counting.count_consonants()}
-			Total vowels: {counting.count_vowels()}
+			total consonants: {counting.count_consonants()}
+			total vowels: {counting.count_vowels()}
 			''')
 		else:
 			print('Goodbye!')
@@ -43,3 +44,32 @@ def main():
 
 if __name__ == '__main__':
 	main()
+#
+## functional programimg
+#
+# def sentence_user():
+# 	text = none
+# 	while not text:
+# 		text = input('insert here a text, or a word: ').lower()
+# 	return text
+#
+#
+# consonants = 'bcdfghjklmnpqrstvwxyz'
+# vowels = 'aeiou'
+#
+# print('to quit insert quit below or just continue to use the program.')
+# while true:
+# 	counter_vowels, counter_consonants = 0, 0
+# 	sentence = sentence_user()
+# 	if sentence != 'quit':
+# 		for letter in sentence:
+# 			if letter in consonants:
+# 				counter_consonants += 1
+# 			if letter in vowels:
+# 				counter_vowels += 1
+# 		print(f'''
+# 			 total consonants: {counter_consonants}
+# 			 total vowels: {counter_vowels}''')
+# 	else:
+# 		print('goodbye!')
+# 		break
