c09f76aba9b586f1f0b27356b5684c31d147b851
Marinel-Neagu <neagumarinel2014@gmail.com>
2023-09-13 18:19:23 +0300

    Need to polish the program

--- Sudoku_Solver_UI.py
+++ Sudoku_Solver_UI.py
@@ -1,49 +1,57 @@
-BOARD = [[7, 8, 0, 4, 0, 0, 1, 2, 0], [6, 0, 0, 0, 7, 5, 0, 0, 9], [0, 0, 0, 6, 0, 1, 0, 7, 8],
-         [0, 0, 7, 0, 4, 0, 2, 6, 0], [0, 0, 1, 0, 5, 0, 9, 3, 0], [9, 0, 4, 0, 6, 0, 0, 0, 5],
-         [0, 7, 0, 3, 0, 0, 0, 1, 2], [1, 2, 0, 0, 0, 7, 4, 0, 0], [0, 4, 9, 2, 0, 6, 0, 0, 7]]
+GRID = [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 
-class SudokuSolver:
-	def __init__(self, board):
-		self.board = board
-	
-	def print_board(self):
-		for row in range(len(self.board)):
-			if row % 3 == 0 and row != 0:
-				print('_ _ _ _ _ _ _ _ _ _ ')
-			for column in range(len(self.board[0])):
-				if column % 3 == 0 and column != 0:
-					print('|', end='')
-				if column != 8:
-					print(self.board[row][column], end=' ')
-				else:
-					print(self.board[row][column])
-	
-	def find_empty(self):
-		for row in range(len(self.board)):
-			for column in range(len(self.board[0])):
-				if self.board[row][column] == 0:
-					return row, column
-		return None
+def print_board(board):
+	for row in range(0, 9):
+		if row % 3 == 0 and row != 0:
+			print('_ _ _ _ _ _ _ _ _ _ _ _')
+		for column in range(0, 9):
+			if column % 3 == 0 and column != 0:
+				print("|", end=' ')
+			if column == 8:
+				print(board[row][column])
+			else:
+				print(board[row][column], end=' ')
 
-	def solved(self):
-		find = self.find_empty()
-		if find:
-			row, column = find
-		else:
-			return True
-			
+
+def find_empty(board):
+	for row in range(0, 9):
+		for column in range(0, 9):
+			if board[row][column] == 0:
+				return row, column
+	return None
+
+
+def valid_board(board, number, empty_position):
+	for row in range(0, 9):
+		if board[row][empty_position[1]] == number and empty_position[1] != row:
+			return False
 	
+	for column in range(0, 9):
+		if board[empty_position[0]][column] == number and empty_position[0] != column:
+			return False
 	
-	
-	def valid_board(self):
-		for row in range(len(self.board)):
-			if self.board[]
-			
+	box_row = empty_position[0] // 3
+	box_column = empty_position[0] // 3
+	for row in range(box_row * 3, box_row * 3 + 3):
+		for column in range(box_column * 3, box_column * 3 + 3):
+			if board[row][column] == number:
+				return False
+	return None
+
+def solve(board):
+	find = find_empty(board)
+	if find:
+		return True
+	else:
+		row,column =find
+		re
+
 
 def main():
-	sudoku = SudokuSolver(board=BOARD)
-	sudoku.print_board()
+	print_board(GRID)
 
 
 if __name__ == '__main__':
