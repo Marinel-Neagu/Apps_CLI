9e778d771e2acadb2f9a7c2ce19921cdf739571f
mary <neagumarinel2014@gmail.com>
2023-10-15 11:34:27 -0400

    .

--- Anagram_Generator_UI.py
+++
@@ -1,6 +0,0 @@
-import itertools
-
-text = 'TEXT:e file_name'
-text_permute = itertools.permutations(text, len(text))
-
-
--- Simple_Sudoku_Solver_UI.py
+++
@@ -1,7 +0,0 @@
-def print_board(board):
-	for column in board:
-		for row in board:
-			print(board[column][row],end=' ')
-		print()
-def main():
-	for column
